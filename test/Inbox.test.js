28201f1ba36ce8a5e8ae916f30fc866007ab7edb
Alex Kibler <alexkibler@me.com>
2018-06-11 09:19:00 -0600

    default message test added

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,6 +8,7 @@ const web3 = new Web3(provider)
 
 const { interface, bytecode } = require('../compile')
 
+const INITIAL_STRING = 'Hi there!'
 
 let accounts
 let inbox
@@ -18,7 +19,7 @@ beforeEach(async () => {
 
   // Use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
     .send({ from: accounts[0], gas: '1000000' })
 
   inbox.setProvider(provider)
@@ -27,5 +28,10 @@ beforeEach(async () => {
 describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address)
+  })
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call()
+    assert.equal(message, INITIAL_STRING)
   })
 })
