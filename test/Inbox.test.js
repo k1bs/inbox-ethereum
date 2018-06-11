acebdcdcd483cf4ccd9dac1b69f43bf57abead9c
Alex Kibler <alexkibler@me.com>
2018-06-11 09:13:17 -0600

    bug fixes

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,10 +3,11 @@
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
+const provider = ganache.provider()
+const web3 = new Web3(provider)
 
 const { interface, bytecode } = require('../compile')
 
-const web3 = new Web3(ganache.provider())
 
 let accounts
 let inbox
@@ -19,6 +20,8 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' })
+
+  inbox.setProvider(provider)
 })
 
 describe('Inbox', () => {
